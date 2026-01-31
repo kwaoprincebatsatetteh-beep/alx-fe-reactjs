74a1562d2b8f0889ade44cd72c4b33b02f8d10ba
https://github.com/kwaoprincebatsatetteh-beep/alx-fe-reactjs.git <kwaoprincebatsatetteh@gmail.com>
2026-01-31 01:12:23 +0100

    c

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,9 +1,12 @@
+
 import { useState } from "react";
 import fetchUserData from "../services/githubService";
 
 function Search() {
   const [username, setUserName] = useState("")
-  const [user, setUser] = useState(null);
+  const [location, setLocation] = useState("");
+  const [repoCount, setRepoCount] = useState("")
+  const [user, setUser] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(false)
 
@@ -13,8 +16,8 @@ function Search() {
     setError(false)
     
     try{  
-    const data = await fetchUserData(username);
-    setUser(data);
+    const data = await fetchUserData(username, location, repoCount);
+    setUser([...user, ...data.items]);
 
     console.log(user)
 
@@ -28,20 +31,48 @@ function Search() {
 
   return(
     <form onSubmit={handleSubmit} >
-      <input type="text"
-       placeholder="Enter github username here...." 
-       value={username}
-       onChange={(e) => setUserName(e.target.value)}/>
-      <button type="submit">Search</button>
+      <div className="flex flex-col gap-y-5 items-center w-50 my-0 mx-auto">
+        <div>
+          <label htmlFor="user-name">Username</label>
+          <input type="text"
+           placeholder="Enter github username here...."
+           value={username}
+           onChange={(e) => setUserName(e.target.value)} className="border w-100" id="user-name"/>
+        </div>
+
+         <div>
+          <label htmlFor="location">Location</label>
+          <input type="text"
+           placeholder="Enter user location" 
+           className="border w-100" 
+           id="location"
+           value={location}
+           onChange={(e) => setLocation(e.target.value)}
+           />
+          </div>
+  
+         <div>
+          <label htmlFor="repo-count">Repository Count</label>
+          <input type="text"
+           className="border w-100" 
+           id="repo-count"
+           value={repoCount}
+           onChange={(e) => setRepoCount(e.target.value)}
+           />
+          </div>
+
+          <button type="submit" className="m-auto border px-4 py-2 bg-black text-white cursor-pointer">Search</button>
+      </div>
+     
 
 
       {isLoading && <p>Loading....</p>}
       {error && <p>Looks like we cant find the user</p>}
       {user && (
-        <div>
-          <img src={user.avatar_url} alt="" />
-          <h2>{user.login}</h2>
-          <a href={user.html_url}>View profile</a>
+        <div className="my-19 mx-auto w-50">
+          <img src={user.avatar_url} alt="" className="w-50"/>
+          <h2 className="">{user.login}</h2>
+          <a href={user.html_url} className="text-blue-600">View profile</a>
         </div>
       )
       }
